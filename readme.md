984229cbefd2b5a2cc68f8ff4356a950430a1627
Chris Johnson <chrisltd@gmail.com>
2013-02-22 21:59:30 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,60 @@
+# Yo Faux Pagination
+Yo Faux Pagination is a JQuery plugin that will let you page through a set of page elements by hiding and showing them in order. Just activate the plugin on a wrapper element and it will do the work.
+
+Yo Faux Pagination will not activate if there is not more than one child object in the target wrapper.
+
+## Usage Examples
+Simple example
+```
+<div class="faux_paginate">
+  <div>
+    Page 1
+  </div>
+  <div>
+    Page 2
+  </div>
+</div>
+
+<!-- Include JQuery Core above this line -->
+<script src="yofauxpagination.jquery.js"></script>
+<script>
+$(".faux_paginate").YoFauxPagination();
+</script>
+```
+
+Advanced example with callbacks
+```
+<form class="paginate_form">
+  <div class="paginate_page">
+    <label>Input 1</label>
+    <input>
+  </div>
+  <div class="paginate_page">
+    <label>Input 2</label>
+    <input>
+  </div>
+  <input type="submit" value="Submit">
+</form>
+
+<!-- Include JQuery Core above this line -->
+<script src="yofauxpagination.jquery.js"></script>
+<script>
+$(".paginate_form").YoFauxPagination({
+  'childObject'         : '.paginate_page',
+  'initCallback' : function(){ $('.paginate_form > [type="submit"]').hide(); }, // Hide submit form
+  'lastCallback' : function(){ $('.paginate_form > [type="submit"]').show(); } 
+});
+</script>
+```
+
+## Options
+```
+'childObject'         : 'div',                // Target object
+'buttonType' : 'button',                  // Next and Previous button tag
+'prevText' : 'Previous',                     // Text for the previous button
+'nextText' : 'Next',                          // Text for the next button
+'initCallback' : function() {},            // Called if plugin initialized on an object
+'nextCallback' : function() {},           // Called after the next button is pressed
+'previousCallback' : function() {},     // Called after the previous button is pressed
+'lastCallback' : function() {}              // Called when the user reaches the last "page"
+```
---
+++ yofauxpagination.jquery.js
@@ -0,0 +1,75 @@
+// By Chris Johnson
+// http://chrisltd.com
+// Created February 2013
+
+// Run this plugin on a wrapper, optionally define a child object (default's to DIV) to paginate on. This will hide all the subobjects and provide buttons for advancing and backing through them (default's to BUTTONS). You can provide a callback function for when the last object is reached.
+
+(function( $ ){
+
+  $.fn.YoFauxPagination = function( options ) {  
+
+    // Create some defaults, extending them with any options that were provided
+    var settings = $.extend( {
+      'childObject'         : 'div',                // Target object
+      'buttonType' : 'button',                  // Next and Previous button tag
+      'prevText' : 'Previous',                     // Text for the previous button
+      'nextText' : 'Next',                          // Text for the next button
+      'initCallback' : function() {},            // Called if plugin initialized on an object
+      'nextCallback' : function() {},           // Called after the next button is pressed
+      'previousCallback' : function() {},     // Called after the previous button is pressed
+      'lastCallback' : function() {}              // Called when the user reaches the last "page"
+    }, options);
+
+    // Plugin code
+    return this.each(function(index, value) {        
+
+      // Find and count child objects
+      var childTotal = $("> " + settings.childObject, this).length;
+      if(childTotal == 0){
+        return;
+      }
+      settings.initCallback();
+
+      // Modify child objects
+      $("> " + settings.childObject, this).each(function(index, value) {     
+        $(this).attr("data-total", childTotal);
+        $(this).attr("data-target", index);
+        $(this).append('<div class="paginate_nav" />');
+        nav = $(".paginate_nav", this);
+        //  don't hide the first page or put a previous button
+       if( index !== 0 ){
+          $(nav).append('<' + settings.buttonType + ' class="paginate_previous" data-target-index="' + (index - 1) + '">' + settings.prevText + '</' + settings.buttonType + '>');
+          $(this).hide();
+        }
+        // Every object but the last gets a next button
+        if (index !== childTotal - 1) {
+          $(nav).append('<' + settings.buttonType + ' class="paginate_next" data-target-index="' + (index + 1) + '">' + settings.nextText + '</' + settings.buttonType + '>');
+        }
+
+        // Bind next button
+        $("> .paginate_nav > .paginate_next", this).click(function(event) {
+          event.preventDefault();
+          $(this).parent().parent().hide();
+          $('[data-target="' + $(this).data("target-index") + '"]').show();
+          settings.nextCallback();
+          // Check to see if this is the last page, if so callback time
+          if( ($(this).parent().parent().data("total") - 1) == $(this).data("target-index")){
+            settings.lastCallback();
+          }
+        });
+
+        // Bind previous button
+        $("> .paginate_nav > .paginate_previous", this).click(function(event) {
+          event.preventDefault();
+          $(this).parent().parent().hide();
+          $('[data-target="' + $(this).data("target-index") + '"]').show();
+          settings.nextCallback();
+        });
+
+      });
+
+    });
+
+
+  };
+})( jQuery );
